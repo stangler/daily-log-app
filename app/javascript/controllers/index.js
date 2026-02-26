e94032be462af06333e0825cb6efc6bbfc72ce3e
sub <sub@mail.com>
2026-02-26 02:26:56 +0000

    Complete Phase9

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,6 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+// Import additional dependencies
+import 'marked'
